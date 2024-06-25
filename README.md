5a56317ce94c213d85768da4d6025e6d4b53a35f
Gonçalo Miguel <58638436+GugaaLx@users.noreply.github.com>
2024-06-25 01:50:42 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# MERN-taskmanager
+ This repository hosts a project designed to explore and master MERN (MongoDB, Express, React, Node.js) technology through the development of an application that serves a meaningful purpose for society. The primary objective is to gain practical experience with the MERN stack while building a useful and impactful solution.
---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,32 @@
+import React, { useEffect } from 'react'
+import { useForm } from "react-hook-form"
+import { useNavigate } from 'react-router-dom'
+
+const Login = () => {
+
+  const user = '' 
+  const {register, handleSubmit, formState:{errors}} = useForm()
+
+  const navigate = useNavigate()
+
+  useEffect (() => {
+    user &&  navigate('/dashboard')
+  }, [user])
+
+  return (
+    <div className='w-full min-h-screen flex items-center justify-center flex-col lg:flex-row bg-[#f3f4f6]'>
+      <div className='w-full md:w-auto flex gap-0 md:gap-40 flex-col md:flex-row items-center justify-center'>
+        <div className='h-full w-full lg:w-2/3 flex flex-col items-center justify-center'>
+
+        </div>
+        <div>
+
+        </div>
+
+      </div>
+       
+    </div>
+  )
+}
+
+export default Login
