0329434f79e1cbe391d4b6fcc7651d2435655d7b
Gonçalo Miguel <58638436+GugaaLx@users.noreply.github.com>
2024-06-26 02:32:51 +0100

    setting redux

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
-import { Navigate, Route, Routes, useLocation } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+import { Navigate, Outlet, Route, Routes, useLocation } from 'react-router-dom'
 import { Toaster } from 'sonner'
 import Dashboard from './pages/Dashboard'
 import Login from './pages/Login'
@@ -8,24 +9,24 @@ import Trash from './pages/Trash'
 import Users from './pages/Users'
 
 function Layout () {
-  const User = '' 
+  const {user} = useSelector((state) => state.auth) 
 
   const location = useLocation()
 
   return user ? (
     <div className='w-full h-min-screen flex flex-col md:flex-row'>
       <div className='w-1/5 h-screen bg-white sticky top-0 hidden md:block'>
-        {/* <Sidebar /> */}
+        <Sidebar />
       </div>
 
       {/* <MobileSidebar/> */}
 
       <div className='flex-1 overflow-y-auto'>
         {/* <Navbar/> */}
-      <div className='p-4 2xl:px-10 '>
-        {/* <Outlet /> */}
+        <div className='p-4 2xl:px-10 '>
+          <Outlet />
         </div>
-        </div>
+      </div>
     </div>
 
   ): (
@@ -39,7 +40,7 @@ function App() {
     <main className='w-full min-h-screen bg-[#f3f4f6]'>
       <Routes>
         <Route element={<Layout/>}>
-          <Route path='/' element={<Navigate to='/dashboard'/>} />
+          <Route index path='/' element={<Navigate to='/dashboard'/>} />
           <Route path='/dashboard' element={<Dashboard />} />
           <Route path='/tasks' element={<Tasks />} />
           <Route path='/completed/:status' element={<Tasks />} />
